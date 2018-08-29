81a3e1159edef1c56c6f73f8c4ad70ffcec670cc
thienJs <cristian.albarran95@gmail.com>
2018-08-28 23:59:56 -0400

    Project Structure

---
+++ app.js
@@ -0,0 +1,25 @@
+// Budget Controller
+const budgetController = (() => {
+
+})();
+
+// UI Controller
+const UIController = (() => {
+
+})();
+
+//  GLOBAL APP CONTROLLER
+const controller = ((budgetCtrl, UICtrl) => {
+  document.querySelector('.add__btn').addEventListener('click', () => {
+    // get field input data
+
+    //  add item to the budget controller
+
+    // add the item to UI
+
+    // calculate the budget
+
+    // display budget on the UI
+  });
+
+})(budgetController, UIController);
