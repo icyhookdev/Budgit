cc0e7a77e613a6961b5acfcc149067ab80a6bf93
thienJs <cristian.albarran95@gmail.com>
2018-08-29 19:04:52 -0400

    ctrlAddItem function added

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const UIController = (() => {
 
 //  GLOBAL APP CONTROLLER
 const controller = ((budgetCtrl, UICtrl) => {
-  document.querySelector('.add__btn').addEventListener('click', () => {
+
+  const ctrlAddItem = () => {
+
     // get field input data
 
     //  add item to the budget controller
@@ -20,6 +22,16 @@ const controller = ((budgetCtrl, UICtrl) => {
     // calculate the budget
 
     // display budget on the UI
+    console.log('hi there');
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  // add when the return key is press
+  document.addEventListener('keypress', (e) => {
+    if(e.keyCode === 13 || e.which === 13){
+      ctrlAddItem();
+    }
   });
 
 })(budgetController, UIController);
