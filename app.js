67e0185ca14955a1093628e4b73414f94f68c2d2
thienJs <cristian.albarran95@gmail.com>
2018-08-29 20:24:42 -0400

    UIController setup

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const budgetController = (() => {
 
 // UI Controller
 const UIController = (() => {
+  const DOMstring = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    addButton: '.add__btn'
+  }
+
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMstring.inputType).value,
+        description: document.querySelector(DOMstring.inputDescription).value,
+        value: document.querySelector(DOMstring.inputValue).value
+      }
+    },
+    getDomStrings: () => {
+      return DOMstring;
+    }
+  }
 
 })();
 
 //  GLOBAL APP CONTROLLER
 const controller = ((budgetCtrl, UICtrl) => {
+  const DOM = UICtrl.getDomStrings();
 
   const ctrlAddItem = () => {
-
     // get field input data
+      const input = UICtrl.getInput();
 
+      console.log(input);
     //  add item to the budget controller
 
     // add the item to UI
@@ -22,10 +43,9 @@ const controller = ((budgetCtrl, UICtrl) => {
     // calculate the budget
 
     // display budget on the UI
-    console.log('hi there');
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.addButton).addEventListener('click', ctrlAddItem);
 
   // add when the return key is press
   document.addEventListener('keypress', (e) => {
